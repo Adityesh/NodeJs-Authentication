bd0a2e17aec074d332fd90b41f886f2f09f85aba
Adityesh <adityeshmishra98@gmail.com>
2019-08-21 22:25:19 +0530

    Validation for Registration JOI

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+//Import Routes
+const authRoute = require('./routes/auth')
 
 dotenv.config();
 
 //COnnect to database
 mongoose.connect(process.env.DB_CONNECT,{ useNewUrlParser: true },()=> console.log("COnencted"))
 
-//Import Routes
-const authRoute = require('./routes/auth')
+//Middleware
+app.use(express.json());
 
 //Route Middleware
 app.use('/api/user',authRoute);
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,28 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const {registerValidation} = require('../validation')
 
-router.post('/register',(req,res) =>{
+
+router.post('/register',async (req,res) =>{
+
+    //Validate an user before submitting
+    const {error} = registerValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message)
+
+
     const user = new User({
         name : req.body.name,
         email : req.body.email,
         password : req.body.password,
         date : req.body.date
     })
+
+    try{
+        const savedUser = await user.save();
+        res.send(savedUser)
+    }catch(err){
+        res.status(400).send(err);
+    }
 })
 
 module.exports = router;
